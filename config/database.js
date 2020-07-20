60489281c7b0847257e5d3fdfc01ee1c2c1acb42
Cristiano <cristianosantosesilva@gmail.com>
2020-07-20 19:10:04 -0300

    Concluindo configuração mongoDB e logs

--- config/database.js
+++ config/database.js
@@ -1,3 +1,18 @@
-var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://locahost/apitrainer');
+module.exports = uri => {
+    
+    var mongoose = require('mongoose');
+    
+    mongoose.connect('mongodb://' + uri);
+    
+    mongoose.connection.on('connected', () => console.log('Conectado ao mongoDB'));
+    
+    mongoose.connection.on('error', error => console.log('Erro na conexão com o mongoDB ' + error));
+    
+    mongoose.connection.on('disconnected', () => console.log('mongodb foi desconectado'));
+    
+    process.on('SIGINT', () => mongoose.connection.close(() => {
+        console.log('Conexão com mongoDB encerrada pelo termino do app')
+        process.exit(0);
+    }));
+}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express');
-require('./config/database');
+require('./config/database')('localhost/apitrainer');
 
 http.createServer(app)
     .listen('3000', () => console.log('Started server'));
