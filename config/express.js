1581c04576bf915ef5dd0641cf4b944dd742a58f
Cristiano <cristianosantosesilva@gmail.com>
2020-07-20 18:45:00 -0300

    adicionando e configurando mongoose

--- config/express.js
+++ config/express.js
@@ -3,6 +3,7 @@ var app = express();
 var consign = require('consign');
 var bodyParser = require('body-parser');
 
+
 app.use(bodyParser.json());
 
 consign({cwd: 'app'})
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express');
-
+require('./config/database');
 
 http.createServer(app)
     .listen('3000', () => console.log('Started server'));
